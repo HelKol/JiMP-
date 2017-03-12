3e79730b642e35bcc132ce80643e7e78234ab455
GSxxx <infopreenk@gmail.com>
2017-03-12 23:15:53 +0000

    lab2 greatesproduct inp

--- lab2/greatestproduct/GreatestProduct.cpp
+++ lab2/greatestproduct/GreatestProduct.cpp
@@ -4,22 +4,39 @@
 
 #include "GreatestProduct.h"
 
-int GreatestProduct(const std::vector<int> &numbers, int k)
-{
-    int a=-1,b=-1;
-    //po obiekcie vector można iterować w pętli for
+int GreatestProduct(const std::vector<int> &numbers, int k) {
+    std::vector<int> tmp;
+    int max= 1;
     for (int v : numbers) {
-        if(v> a)
-        {
-            if(v>b)
-            {
-                b=v;
-            }
-            else
-            {
-                a=v;
+        tmp.push_back(v);
+        if(tmp.size()== k){
+            for (int v : tmp) {
+                max*= v;
             }
         }
-    }
-    return a*b;
+        if(tmp.size()>k){
+            int to_erase= -1;
+            for (int i=0;i<k;i++){
+                int max_tmp= 1;
+                for (int d=0; d<k;d++){
+                    if(d==i){
+                        max_tmp*= tmp[k];
+                    } else {
+                        max_tmp+= tmp[d];
+                    }
+                }
+                if (max_tmp> max){
+                    max= max_tmp;
+                    to_erase= i;
+                }
+            }
+            if (to_erase!=-1)
+                tmp.erase(tmp.begin()+to_erase);
+            else
+                tmp.pop_back();
+            }
+        }
+
+
+    return max;
 }
