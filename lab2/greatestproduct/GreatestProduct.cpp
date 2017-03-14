c01053b21e1abf159e5d5d7b3b066ed4c04707d6
HelKol <hela1335@gmail.com>
2017-03-14 15:43:07 +0100

    still doesnt work

--- lab2/greatestproduct/GreatestProduct.cpp
+++ lab2/greatestproduct/GreatestProduct.cpp
@@ -6,36 +6,65 @@
 #include <vector>
 
 int GreatestProduct(const std::vector<int> &numbers, int k) {
-    std::vector<int> thebiggest(k);
-    std::vector<int> theleast(k);
-    for (int i =0;i<numbers.size();i++) {
+    std::vector<int> thebiggest;
+    std::vector<int> theleast;
+    for (int i =0;i<=numbers.size();i++) {
         int element = numbers[i];
         if (i<k) {
             if (i == 0) {
                 thebiggest.push_back(element);
                 theleast.push_back(element);
             } else {
-                for (int j = 0; j <= i; j++) {
-                    if (element > thebiggest[j]) {
-                        thebiggest[j + 1] = thebiggest[j];
-                        thebiggest[j] = element;
-                    }
-                    if (element < theleast[j]) {
-                        theleast[j + 1] = theleast[j];
-                        theleast[j] = element;
-                    }
+                int flag1=0;
+                int flag2=0;
+                if (element<*thebiggest.end()) {
+                    thebiggest.push_back(element);
+                    for (int j = 0; j <= i; j++) {
+                        if (element < theleast[j] && !flag2) {
+                            theleast.insert(theleast.begin() + j, numbers[i]);
+                            flag2 = 1;
+                        }
 
+                    }
+                }
+                else if(element>*theleast.end()) {
+                    theleast.push_back(element);
+                    for (int j = 0; j <= i; j++) {
+                        if (element > thebiggest[j] && !flag1) {
+                            thebiggest.insert(thebiggest.begin()+j, numbers[i]);
+                            flag1=1;
 
+                        }
+                    }
+                }
+                else{
+                    for (int j = 0; j <= i; j++) {
+                        if (element > thebiggest[j] && !flag1) {
+                            thebiggest.insert(thebiggest.begin()+j, numbers[i]);
+                            flag1=1;
+
+                        }
+                        if (element < theleast[j] && !flag2) {
+                            theleast.insert(theleast.begin()+j,numbers[i]);
+                            flag2=1;
+                        }
+
+                    }
                 }
             }
         }
         else{ //rozwazamy przypadek jak i>k, czyli porownujemy z istniejacymi najwiekszmi liczbami i jesli trzeba je wyrzucamy
             if (thebiggest[k-1]<element) {
                 thebiggest.pop_back();
-                for (int l = 0; l <= k - 1; l++) {
-                    if (element > thebiggest[l]) {
-                        thebiggest[l + 1] = thebiggest[l];
-                        thebiggest[l] = element;
+                if (element<thebiggest[k-2])
+                    thebiggest.push_back(element);
+                else
+                {
+                    for (int l = k-2; l >= 0; l--) {
+                        if (element < thebiggest[l]) {
+                            thebiggest.insert(thebiggest.end()-l+1, numbers[i]);
+                            break;
+                        }
                     }
                 }
             }
@@ -56,10 +85,10 @@ int GreatestProduct(const std::vector<int> &numbers, int k) {
     }
     int product1=1;
     int product2=1;
-    for (int v : thebiggest)
-        product1=product1*v;
-    for (int w : theleast)
-        product2=product2*w;
+    for (int v=0;v<k;v++)
+        product1=product1*thebiggest[v];
+    for (int w=0;w<k;w++)
+        product2=product2*theleast[w];
 
     if (product2>product1)
         return product2;
