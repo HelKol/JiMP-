53e1b2654709165000946f42efb74dc658c3a591
GSxxx <infopreenk@gmail.com>
2017-03-14 18:02:57 +0100

    lab3

--- lab3/ccounter/CCounter.cpp
+++ lab3/ccounter/CCounter.cpp
@@ -7,11 +7,21 @@
 #include "CCounter.h"
 
 
+namespace ccounter {
 
-std::unique_ptr <Counter> Init();
+    std::unique_ptr<Counter> Init() {
+        return make_unique<Counter>();
+    }
 
-void Inc(std::string key, std::unique_ptr <Counter> *counter);
+    void Inc(std::string key, std::unique_ptr<Counter> *counter) {
 
-int Counts(const std::unique_ptr <Counter> &counter, std::string key);
+    }
 
-void SetCountsTo(std::string key, int value, std::unique_ptr <Counter> *counter);
+    int Counts(const std::unique_ptr<Counter> &counter, std::string key) {
+        return 0;
+    }
+
+    void SetCountsTo(std::string key, int value, std::unique_ptr<Counter> *counter) {
+
+    }
+}
