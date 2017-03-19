9833308b4aeda36d7685cebc8e08ecc623868886
GSxxx <prenc@student.agh.edu.pl>
2017-03-19 23:30:09 +0100

    smartTree 3/5 test + having fun with withe spaces

--- lab3/smarttree/SmartTree.cpp
+++ lab3/smarttree/SmartTree.cpp
@@ -13,33 +13,47 @@ namespace datastructures{
 
     std::unique_ptr<SmartTree>
     InsertLeftChild(std::unique_ptr<SmartTree> tree, std::unique_ptr<SmartTree> left_subtree) {
-        if(left_subtree->left!= nullptr){
-            auto tmp= move(left_subtree->left);
-            left_subtree->left= move(tree);
-            tree->left= move(tmp);
-        } else {
-            left_subtree->left= move(tree);
+        if(tree->left!= nullptr) {
+            auto tmp = move(tree->left);
+            left_subtree->left = move(tmp);
         }
-
-        return left_subtree;
+        tree->left= move(left_subtree);
+        return tree;
     }
 
     std::unique_ptr<SmartTree>
     InsertRightChild(std::unique_ptr<SmartTree> tree, std::unique_ptr<SmartTree> right_subtree) {
-        auto tmp= move(right_subtree->left);
-        right_subtree->left= move(tree);
-        tree->left= move(tmp);
-        return std::unique_ptr<SmartTree>();
+        if(tree->right!= nullptr) {
+            auto tmp = move(tree->right);
+            right_subtree->right = move(tmp);
+        }
+        tree->right= move(right_subtree);
+        return tree;
     }
 
     void PrintTreeInOrder(const std::unique_ptr<SmartTree> &unique_ptr, std::ostream *out) {
-
+        if(unique_ptr!= nullptr){
+            PrintTreeInOrder(unique_ptr->left, out);
+            *out<<unique_ptr->value<<", ";
+            PrintTreeInOrder(unique_ptr->right, out);
+        }
     }
 
     std::string DumpTree(const std::unique_ptr<SmartTree> &tree) {
-        if(tree->left!= nullptr) DumpTree(tree->left);
-        if(tree->right!= nullptr) DumpTree(tree->right);
-//        tree.release();
+        std::string output;
+        if(tree->left!= nullptr) {
+            output+= DumpTree(tree->left)+" ";
+        } else {
+            output+= "[none]";
+        }
+        if (tree->right!= nullptr){
+            output+= DumpTree(tree->right);
+        } else{
+            output+=" [none]";
+        }
+
+        output="["+std::to_string(tree->value)+" "+output+"]";
+        return output;
     }
 
     std::unique_ptr<SmartTree> RestoreTree(const std::string &tree) {
--- lab3/smarttree/main.cpp
+++ lab3/smarttree/main.cpp
@@ -1,4 +1,14 @@
 //
 // Created by xxx on 19.03.17.
 //
+#include <iostream>
+#include "SmartTree.h"
+using namespace datastructures;
 
+int main(void){
+    auto root = CreateLeaf(99);
+    root = InsertLeftChild(move(root), CreateLeaf(100));
+    root = InsertRightChild(move(root), CreateLeaf(88));
+
+    std::cout<<DumpTree(root);
+}
