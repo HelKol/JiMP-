4dd693205cb61248852f461cf051448c6621f1db
GSxxx <prenc@student.agh.edu.pl>
2017-03-26 23:32:11 +0200

    SImpleUrl Completed

--- lab4/netsurl/SimpleUrl.cpp
+++ lab4/netsurl/SimpleUrl.cpp
@@ -7,7 +7,7 @@ namespace nets{
     std::string SimpleUrl::Login() const {
         std::smatch matches;
         if (regex_match(url, matches, regex)){
-            return matches[3];
+            return matches[SimpleUrl::login];
         }
 
     }
@@ -15,42 +15,49 @@ namespace nets{
     std::string SimpleUrl::Host() const {
         std::smatch matches;
         if (regex_match(url, matches, regex)){
-            return matches[3];
+            return matches[SimpleUrl::host];
         }
     }
 
     std::string SimpleUrl::Path() const {
         std::smatch matches;
         if (regex_match(url, matches, regex)){
-            return matches[3];
+            return matches[SimpleUrl::path];
         }
     }
 
     uint16_t SimpleUrl::Port() const {
         std::smatch matches;
+
         if (regex_match(url, matches, regex)){
-            return matches[3];
+            if(matches[SimpleUrl::port]!=""){
+                return stoi(matches[SimpleUrl::port]);
+            }
+            else if(matches[SimpleUrl::sheme]=="https") return 443;
+            else if (matches[SimpleUrl::sheme]=="http") return 80;
+            else if (matches[SimpleUrl::sheme]=="ftp") return 22;
+            else return 0;
         }
     }
 
     std::string SimpleUrl::Scheme() const {
         std::smatch matches;
         if (regex_match(url, matches, regex)){
-            return matches[1];
+            return matches[SimpleUrl::sheme];
         }
     }
 
     std::string SimpleUrl::Query() const {
         std::smatch matches;
         if (regex_match(url, matches, regex)){
-            return matches[3];
+            return matches[SimpleUrl::query];
         }
     }
 
     std::string SimpleUrl::Fragment() const {
         std::smatch matches;
         if (regex_match(url, matches, regex)){
-            return matches[3];
+            return matches[SimpleUrl::fragment];
         }
     }
 }
--- lab4/netsurl/SimpleUrl.h
+++ lab4/netsurl/SimpleUrl.h
@@ -11,19 +11,20 @@
 
 namespace nets{
     class SimpleUrl {
-        std::string url;
-        std::regex regex {R"(^([a-zA-z]+):(\/\/)?([a-z\.-]+)(\/[^#?\n]*)?(\?([^#\n]+))?(#([^\n]+)?)?(:([0-9]+))?)"};
-        SimpleUrl(std::string value){
-            url= value;
-        }
-        std::string Login() const;
-        std::string Host() const;
-        std::string Path() const;
-        uint16_t Port() const;
-        std::string Scheme() const;
-        std::string Query() const;
-        std::string Fragment() const;
-
+        public:
+            enum trelemorele{sheme= 1, login= 4, host= 5, port= 7, path= 9, query= 11, fragment= 13};
+            std::string url;
+            std::regex regex {R"(^([a-zA-z]+):(\/\/)?(([a-z\.]+)@)?([a-z\.-]+)(:([0-9]+))?(\/([^#?\n]*))?(\?([^#\n]+))?(#([^\n]+)?)?)"};
+            SimpleUrl(std::string value){
+                url= value;
+            }
+            std::string Login() const;
+            std::string Host() const;
+            std::string Path() const;
+            uint16_t Port() const;
+            std::string Scheme() const;
+            std::string Query() const;
+            std::string Fragment() const;
     };
 }
 
