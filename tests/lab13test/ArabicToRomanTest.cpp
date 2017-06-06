4bca374cb2ec62057b8295989f6413ef37f05cb4
prenc <prenc@student.agh.edu.pl>
2017-06-06 17:52:07 +0200

    lab13 test arabic to roman

--- tests/lab13test/ArabicToRomanTest.cpp
+++ tests/lab13test/ArabicToRomanTest.cpp
@@ -19,13 +19,13 @@ class ArabicToRomanTest : public ::testing::Test {
 };
 
 TEST_F(ArabicToRomanTest, ArabicToRomanWorksProperly) {
-    EXPECT_EQ(3, ArabicToRoman("III"));
-    EXPECT_EQ(30, ArabicToRoman("XXX"));
-    EXPECT_EQ(4, ArabicToRoman("IV"));
-    EXPECT_EQ(23, ArabicToRoman("XXIII"));
-    EXPECT_EQ(45, ArabicToRoman("VL"));
+    EXPECT_EQ(3, RomanToArabic("III"));
+    EXPECT_EQ(30, RomanToArabic("XXX"));
+    EXPECT_EQ(4, RomanToArabic("IV"));
+    EXPECT_EQ(23, RomanToArabic("XXIII"));
+    EXPECT_EQ(45, RomanToArabic("VL"));
 }
 
 TEST_F(ArabicToRomanTest, EmptyArabicToRomanWorksProperly) {
-    EXPECT_EQ(0, ArabicToRoman(""));
+    EXPECT_EQ(0, RomanToArabic(""));
 }
--- tests/lab13test/RomanToArabicTest.cpp
+++ tests/lab13test/RomanToArabicTest.cpp
@@ -14,14 +14,14 @@ class RomanToArabicTest : public ::testing::Test {
 
 };
 
-TEST_F(RomanToArabicTest , ArabicToRomanWorksProperly) {
-    EXPECT_EQ("CCCXXXIII", RomanToArabic(333));
-    EXPECT_EQ("XIII", RomanToArabic(13));
-    EXPECT_EQ("DCCLVIII", RomanToArabic(758));
-    EXPECT_EQ("CXXIII", RomanToArabic(123));
-    EXPECT_EQ("MMM",RomanToArabic(3000));
+TEST_F(RomanToArabicTest, ArabicToRomanWorksProperly) {
+    EXPECT_EQ("CCCXXXIII", ArabicToRoman(333));
+    EXPECT_EQ("XIII", ArabicToRoman(13));
+    EXPECT_EQ("DCCLVIII", ArabicToRoman(758));
+    EXPECT_EQ("CXXIII", ArabicToRoman(123));
+    EXPECT_EQ("MMM", ArabicToRoman(3000));
 }
 
-TEST_F(RomanToArabicTest , NullValueArabicToRomanWorksProperly) {
-    EXPECT_EQ("", RomanToArabic(0));
+TEST_F(RomanToArabicTest, NullValueArabicToRomanWorksProperly) {
+    EXPECT_EQ("", ArabicToRoman(0));
 }
