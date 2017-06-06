5a80e25533cdb0777787ea44beb8d9435c089dfc
HelKol <hela1335@gmail.com>
2017-06-06 13:44:21 +0200

    Sequential Id Generator all tests passed

--- lab12/academiasequentialgenerator/SequentialGenerator.h
+++ lab12/academiasequentialgenerator/SequentialGenerator.h
@@ -6,7 +6,20 @@
 #define JIMP_EXERCISES_SEQUENTIALGENERATOR_H
 
 
-class SequentialGenerator {
+template <class Id,class Counter>
+
+
+class SequentialIdGenerator {
+public:
+    SequentialIdGenerator() : counter_(0){};
+    SequentialIdGenerator(Counter counter) : counter_(counter){};
+    Id NextValue(){
+        ++counter_;
+        return counter_ - 1;};
+
+
+private:
+    Counter counter_;
 
 };
 
