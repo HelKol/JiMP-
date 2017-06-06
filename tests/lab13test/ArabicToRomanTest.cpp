4b55e3cef3f26524cf5bf91f1c954587279d412b
prenc <prenc@student.agh.edu.pl>
2017-06-06 17:45:58 +0200

    lab13 test arabic to roman

--- tests/lab13test/ArabicToRomanTest.cpp
+++ tests/lab13test/ArabicToRomanTest.cpp
@@ -2,3 +2,30 @@
 // Created by mwypych on 01.06.17.
 //
 
+//
+// Created by mwypych on 29.05.17.
+//
+#include <string>
+#include <MemLeakTest.h>
+#include <gtest/gtest.h>
+#include <Arabicroman.h>
+
+
+using namespace arabicroman;
+
+
+class ArabicToRomanTest : public ::testing::Test {
+
+};
+
+TEST_F(ArabicToRomanTest, ArabicToRomanWorksProperly) {
+    EXPECT_EQ(3, ArabicToRoman("III"));
+    EXPECT_EQ(30, ArabicToRoman("XXX"));
+    EXPECT_EQ(4, ArabicToRoman("IV"));
+    EXPECT_EQ(23, ArabicToRoman("XXIII"));
+    EXPECT_EQ(45, ArabicToRoman("VL"));
+}
+
+TEST_F(ArabicToRomanTest, EmptyArabicToRomanWorksProperly) {
+    EXPECT_EQ(0, ArabicToRoman(""));
+}
--- tests/lab13test/RomanToArabicTest.cpp
+++ tests/lab13test/RomanToArabicTest.cpp
@@ -1,4 +1,27 @@
 //
 // Created by mwypych on 01.06.17.
 //
+#include <string>
+#include <MemLeakTest.h>
+#include <gtest/gtest.h>
+#include <Arabicroman.h>
 
+
+using namespace arabicroman;
+
+
+class RomanToArabicTest : public ::testing::Test {
+
+};
+
+TEST_F(RomanToArabicTest , ArabicToRomanWorksProperly) {
+    EXPECT_EQ("CCCXXXIII", RomanToArabic(333));
+    EXPECT_EQ("XIII", RomanToArabic(13));
+    EXPECT_EQ("DCCLVIII", RomanToArabic(758));
+    EXPECT_EQ("CXXIII", RomanToArabic(123));
+    EXPECT_EQ("MMM",RomanToArabic(3000));
+}
+
+TEST_F(RomanToArabicTest , NullValueArabicToRomanWorksProperly) {
+    EXPECT_EQ("", RomanToArabic(0));
+}
